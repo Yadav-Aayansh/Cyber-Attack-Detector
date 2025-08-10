e851b27ccd264b3bbf259b687d32aa972de82437
Yadav Aayansh <aayanshyadav@proton.me>
2025-08-10 22:48:34 +0530

    updated base url cyberdetect

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Cyber-Attack-Detector/',
+  base: '/cyberdetect/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
